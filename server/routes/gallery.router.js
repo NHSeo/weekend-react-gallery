54d317b85a46efe46ca64c0ffb4c4086d4c1bdd0
Noah Seo <sht6205@gmail.com>
2024-06-17 18:01:46 -0500

    Sample router js is updated

--- server/routes/gallery.router.js
+++ server/routes/gallery.router.js
@@ -1,14 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../modules/pool.js');
 
 // PUT /gallery/like/:id
 router.put('/like/:id', (req, res) => {
   // code here
+  const galleryId = req.params.id;
+  const sqlText = `
+    UPDATE gallery
+    SET likes = likes + 1
+    WHERE id = $1;
+  `;
+  pool.query(sqlText, [galleryId])
+    .then((dbResult) => {
+      res.sendStatus(200);
+    })
+    .catch((dbError) => {
+      console.log(`PUT Error ${sqlText}`, dbError);
+      res.sendStatus(500);
+    });
 });
 
 // GET /gallery
 router.get('/', (req, res) => {
   // code here
+  const sqlText = `
+    SELECT * FROM gallery
+    ORDER BY id ASC;
+  `;
+  pool.query(sqlText)
+    .then((dbResult) => {
+      res.send(dbResult.rows);
+    })
+    .catch((dbError) => {
+      console.log(`Get Error ${sqlText}`, dbError);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
